8b9c6e208e2e16eea51c6a95e6bb7a70109cc249
DevMohi <morphex72@gmail.com>
2022-05-22 20:37:52 +0600

    connected the database

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mkn0m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connected')
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Manufacturers server')
@@ -9,3 +32,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening to Manufacturers Server ${port}`)
 })
+
+// user:admin
+// password:admin 
