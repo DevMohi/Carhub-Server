d4e5ca236140225cbe963669ae83510759f2a78d
DevMohi <morphex72@gmail.com>
2022-05-22 21:00:08 +0600

    api for getting by id created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const query = require('express/lib/middleware/query');
 require('dotenv').config();
 
 const app = express();
@@ -10,15 +11,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mkn0m.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 async function run() {
     try {
         await client.connect();
-        console.log('connected')
+        console.log('db connected')
+        const partsCollection = client.db("manufacturer").collection("parts");
+
+        app.get('/parts', async (req, res) => {
+            const parts = await partsCollection.find({}).toArray()
+            res.send(parts)
+        })
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const part = await partsCollection.findOne(query)
+            res.send(part)
+        })
     } finally {
 
     }
@@ -33,5 +44,3 @@ app.listen(port, () => {
     console.log(`Listening to Manufacturers Server ${port}`)
 })
 
-// user:admin
-// password:admin 
