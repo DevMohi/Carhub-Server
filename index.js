b3380768a1eb3d1486ac399d785be497defe08ee
DevMohi <morphex72@gmail.com>
2022-05-26 03:52:20 +0600

    api for payment and transaction create

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 app.use(cors());
@@ -23,6 +24,7 @@ async function run() {
         const orderCollection = client.db("manufacturer").collection("orders");
         const reviewCollection = client.db("manufacturer").collection("review");
         const userCollection = client.db("manufacturer").collection("users");
+        const paymentCollection = client.db("manufacturer").collection("payments");
 
         app.get('/parts', async (req, res) => {
             const parts = await partsCollection.find({}).toArray()
@@ -42,6 +44,16 @@ async function run() {
             res.send(part)
         })
 
+        // for managing products as an admin 
+
+        app.delete('/delete-parts/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const result = await partsCollection.deleteOne(query)
+            res.send(result)
+        })
+
         app.get('/orders', verifyJWT, async (req, res) => {
             const email = req.query.email;
             const decodedEmail = req.decoded.email;
@@ -55,11 +67,33 @@ async function run() {
             }
 
         })
+        // getting the orders for payment 
+        app.get('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const order = await orderCollection.findOne(query);
+            res.send(order)
+        })
 
         app.post('/orders', async (req, res) => {
             const orders = req.body;
             const result = await orderCollection.insertOne(orders);
             res.send(result);
+        })
+        // to update if it is paid 
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const result = await paymentCollection.insertOne(payment)
+            const updatedBooking = await orderCollection.updateOne(filter, updatedDoc);
+            res.send(updatedDoc)
         })
 
         app.delete('/orders/:id', async (req, res) => {
@@ -128,6 +162,18 @@ async function run() {
             res.send({ admin: isAdmin })
         })
 
+        // related to stripe 
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.totalPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
 
     } finally {
 
