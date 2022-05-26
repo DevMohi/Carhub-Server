4eba1e3404ae0939c76a92f2029d5477ebe7527b
DevMohi <morphex72@gmail.com>
2022-05-26 18:44:50 +0600

    delete admin order

--- index.js
+++ index.js
@@ -214,6 +214,15 @@ async function run() {
             res.send(updatedDoc)
         })
 
+        //order delete parts
+        app.delete('/order-parts/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
     } finally {
 
     }
